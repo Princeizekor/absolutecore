c2f38ed96aa151d337353d494f988ad1f9576451
Paul Izekor <izekorpaul0@gmail.com>
2023-07-23 04:39:05 +0100

    adding changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,6 @@ function Navbar() {
   const [open, setOpen] = useState(false)
   return (
     <Wrapper>
-      {/* <h1><Link to='/'>OSAS</Link></h1>
-      <ul>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#contact'>Contact</a></li>
-        <li><Link to='#'>Experience</Link></li>
-      </ul> */}
       <a href="#">
         <h1>O<span className="logo">SAS.</span></h1>
       </a>
@@ -26,57 +20,6 @@ function Navbar() {
 }
 
 const Wrapper = styled.div `
-//   position: sticky;
-//   top: 0%;
-//   width: 100%;
-//   height: 80px;
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   padding: 0px 50px;
-//   background-color: black;
-//   z-index: 1000000;
-
-// h1 a{
-//   text-decoration: none;
-//   color: blue;
-// }
-
-// ul {
-//   display: flex;
-//   align-items: center;
-//   gap: 50px;
-//   li {
-//     list-style-type: none;
-//     a {
-//       text-decoration: none;
-//       color: whitesmoke;
-//       font-size: 20px;
-//     }
-//   }
-// }
-// @media (max-width: 768px) {
-//   ul {
-//     position: absolute;
-//     top: 80px;
-//     width: 100%;
-//     height: 20vh;
-//     left: 0%;
-//     align-items: flex-start;
-//     background-color: whitesmoke;
-//     flex-direction: column;
-//     gap: 30px;
-//     padding: 30px;
-//     li {
-//       list-style-type: none;
-//       a {
-//         text-decoration: none;
-//         color: purple;
-//         font-size: 20px;
-//       }
-//     }
-//   }
-// }
   width: 100%;
   display: flex;
   justify-content: space-between;
@@ -87,7 +30,9 @@ const Wrapper = styled.div `
   top: 0%;
   background: black;
   z-index: 1;
-
+a {
+  text-decoration: none;
+}
 .none {
   display: none;
   justify-content: space-between;
